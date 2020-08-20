9ae0a8fa46e36050445d8b1b6f3253a57ae5ede9
LuJunXiongX <1937480486@qq.com>
2020-08-20 17:30:05 +0800

    CAS学习

--- src/com/lujunxiong/thread/AtomicReferenceDemo.java
+++ src/com/lujunxiong/thread/AtomicReferenceDemo.java
@@ -12,19 +12,48 @@ import java.util.concurrent.atomic.AtomicReference;
  * @Description: CAS缺点：循环时间长开销很大；只能保证一个共享变量的原子性；会引出ABA问题
  * ABA：狸猫换太子
  **/
+
 class User{
     private String userName;
     private int age;
 
+    public User(String userName, int age) {
+        this.userName = userName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
 public class AtomicReferenceDemo {
     public static void main(String[] args) {
-//        User zs = new User("z3",22);
-//        User ls = new User("ls", 22);
-//        AtomicReference<User> userAtomicReference = new AtomicReference<>();
-//        userAtomicReference.set(zs);
-//        System.out.println(userAtomicReference.compareAndSet(zs, ls)+"\t"+userAtomicReference.get().toString());
-//        System.out.println(userAtomicReference.compareAndSet(zs, ls)+"\t"+userAtomicReference.get().toString());
+        User zs = new User("zs",22);
+        User ls = new User("ls", 25);
+        AtomicReference<User> userAtomicReference = new AtomicReference<>();
+        userAtomicReference.set(zs);
+        System.out.println(userAtomicReference.compareAndSet(zs, ls)+"\t"+userAtomicReference.get().toString());
+        System.out.println(userAtomicReference.compareAndSet(zs, ls)+"\t"+userAtomicReference.get().toString());
 
     }
 }
